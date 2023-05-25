ca7617b1483ef9b88ea40a92283681b30da3ae6c
Antti Sonkeri <antti.sonkeri@gmail.com>
2023-05-25 22:33:53 +0300

    Avoid running onHover handlers repeatedly

--- src/hooks/useDropCellItem.tsx
+++ src/hooks/useDropCellItem.tsx
@@ -1,6 +1,7 @@
-import { useDrop } from "react-dnd";
+import { DropTargetMonitor, useDrop } from "react-dnd";
 import { CellCoordinates, Item } from "../types";
 import { DragPayload } from "./useDragCellItem";
+import { useState } from "react";
 
 type CollectedProps = {
   isOver: boolean;
@@ -18,18 +19,36 @@ export const useDropCellItem = (
   handleHover: (item: Item, cellOffset: number) => void,
   handleDrop: (item: Item, cellOffset: number) => void
 ) => {
+  const [entered, setEntered] = useState(false);
+
+  // Only trigger hover handler once when entering a cell.
+  const hover = ({ item, cellOffset }: DragPayload) => {
+    if (!entered) {
+      handleHover(item, cellOffset);
+      setEntered(true);
+    }
+  };
+
+  // Collect the `isOver` state for styling the currently hovered drop target.
+  // Reset the `entered` state when the item is no longer over a drop target.
+  const collect = (monitor: DropTargetMonitor<DragPayload>) => {
+    const isOver = !!monitor.isOver();
+    if (!isOver && entered) {
+      setEntered(false);
+    }
+    return {
+      isOver,
+    };
+  };
+
   const [{ isOver }, drop] = useDrop<DragPayload, void, CollectedProps>(
     () => ({
       accept: "CELLITEM",
       drop: ({ item, cellOffset }) => {
         handleDrop(item, cellOffset);
       },
-      hover({ item, cellOffset }) {
-        handleHover(item, cellOffset);
-      },
-      collect: (monitor) => ({
-        isOver: !!monitor.isOver(),
-      }),
+      hover,
+      collect,
     }),
     [cell]
   );
